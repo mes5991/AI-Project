4acec0c680a14db1292b59c9de07ffd72ae5ee84
Dikshya Swain <dswain@wpi.edu>
2016-04-13 17:03:04 -0400

    Added files via upload

--- environment.py
+++ environment.py
@@ -1,15 +1,19 @@
 import numpy as np
 import random
-from robot import Robot
+#from robot import Robot
 
 class Environment():
-
     def __init__(self, envSize, desiredWallPercentage, robotCount, env = None):
-        #envSize = tuple (n, m)
-        #robots = number of robots
+
+        # envSize = tuple (n, m)
+        # robotCount = number of robots
+
         self.robots = []
         self.robotsLocation = []
-        self.envMatrix = self.generateEnv(envSize, desiredWallPercentage, robotCount, env)
+        if env == None:
+            self.envMatrix = self.generateEnv(envSize, desiredWallPercentage, robotCount)
+        else:
+            self.envMatrix = env
 
     def updateEnvMatrix(self, robotIndex, direction):
         oldLoc = self.robotsLocation[robotIndex]
@@ -18,64 +22,64 @@ class Environment():
         self.envMatrix[newLoc[0], newLoc[1]] = 2
         self.robotsLocation[robotIndex] = newLoc
 
-    def generateEnv(self, envSize, desiredWallPercentage, robotCount, env):
+    def generateEnv(self, envSize, desiredWallPercentage, robotCount):
+
         # 0 = empty space
         # 1 = wall or object
         # 2 = robot
 
-        if env == None:
-            #create matirx of empty space
-            envMatrix = np.zeros(envSize, int)
-            colLength = envSize[0]
-            rowLength = envSize[1]
-            rowInd = np.arange(rowLength)
-            colInd = np.arange(colLength)
+        #create matirx of empty space
+        envMatrix = np.zeros(envSize)
+        colLength = envSize[0]
+        rowLength = envSize[1]
+        rowInd = np.arange(rowLength)
+        colInd = np.arange(colLength)
 
-            #add borders
-            envMatrix[0] = 1
-            envMatrix[colLength - 1] = 1
-            for i in range(1, colLength - 1):
-                envMatrix[i, 0] = 1
-                envMatrix[i, rowLength - 1] = 1
+        #add borders
+        envMatrix[0] = 1
+        envMatrix[colLength - 1] = 1
+        for i in range(1, colLength - 1):
+            envMatrix[i, 0] = 1
+            envMatrix[i, rowLength - 1] = 1
 
-            #add random walls
-            wallCount = 0
-            for i in range(0, colLength):
-                for j in range(0, rowLength):
-                    if envMatrix[i, j] == 1:
-                        wallCount += 1
-            desiredWallNum = desiredWallPercentage * (envSize[0] * envSize[1])
-            print("Desired Wall Number: ", desiredWallNum)
-            print("Wall Count: ", wallCount)
-            if wallCount < desiredWallNum:
-                for x in range(0, int(desiredWallNum - wallCount)):
-                    i = random.choice(colInd)
-                    j = random.choice(rowInd)
-                    while envMatrix[i,j] != 0:
-                        i = random.choice(colInd)
-                        j = random.choice(rowInd)
-                    envMatrix[i, j] = 1
+        #add random walls
+        wallCount = 0
+        for i in range(0, colLength):
+            for j in range(0, rowLength):
+                if envMatrix[i, j] == 1:
+                    wallCount += 1
+        desiredWallNum = desiredWallPercentage * (envSize[0] * envSize[1])
+        print("Desired Wall Number: ", desiredWallNum)
+        print("Wall Count: ", wallCount)
+
+        if wallCount < desiredWallNum:
+           for x in range(0, int(desiredWallNum - wallCount)):
+               i = random.choice(colInd)
+               j = random.choice(rowInd)
+               while envMatrix[i,j] != 0:
+                   i = random.choice(colInd)
+                   j = random.choice(rowInd)
+               envMatrix[i, j] = 1
         else:
-            envMatrix = np.array(env)
             envSize = envMatrix.shape
             colLength = envSize[0]
             rowLength = envSize[1]
             rowInd = np.arange(rowLength)
             colInd = np.arange(colLength)
 
-        #add robots
-        for robot in range(0, robotCount):
-            i = random.choice(colInd)
-            j = random.choice(rowInd)
-            while envMatrix[i,j] != 0:
-                i = random.choice(colInd)
-                j = random.choice(rowInd)
-            #Add robot to environment matrix
-            envMatrix[i][j] = 2
-            # envMatrix[8][7] = 2
-            #Store robot world location
-            self.robotsLocation.append([i, j])
-            # self.robotsLocation.append([8, 7])
-            #Initilize robot object and append object to robots list
-            self.robots.append(Robot())
-        return envMatrix
+        # #add robots
+        # for robot in range(0, robotCount):
+        #    i = random.choice(colInd)
+        #    j = random.choice(rowInd)
+        #    while envMatrix[i,j] != 0:
+        #        i = random.choice(colInd)
+        #        j = random.choice(rowInd)
+        #    # Add robot to environment matrix
+        #    envMatrix[i][j] = 2
+        #    envMatrix[8][7] = 2
+        #     #Store robot world location
+        #    self.robotsLocation.append([i, j])
+        #    self.robotsLocation.append([8, 7])
+        # #Initialize robot object and append object to robots list
+        #    self.robots.append(Robot())
+        # return envMatrix
--- main.py
+++ main.py
@@ -1,135 +1,208 @@
 from environment import Environment
 from robot import searchAStar
-import random
 import queue as q
+import xlrd
 import numpy as np
 
-np.set_printoptions(threshold = np.nan, suppress = True, linewidth = 300)
+# Initialize environment (SIZE, Wall%, # of Robots)
 
-"""Code below this line is code for multi-robot search"""
+file_location = 'D:/Academics/Artificial Intelligence/Project/AI-Project-nested_Astar_Search_-No_BFS-/map.xlsx'
+workbook = xlrd.open_workbook(file_location)
+sheet = workbook.sheet_by_index(0)
+env = []
 
-robotCount = 2
-newInfo = []
-stuck = []
+env1 = np.zeros((100, 100))
 
-#Initilize environment (SIZE, Wall%, # of Robots)
-World = Environment((20,20), .50, robotCount)
-print("World Map:\n", World.envMatrix)
-for i in range(robotCount):
-    World.robots[i].updateMap(World.robotsLocation[i], World.envMatrix)
-    World.robots[i].getGoals()
-    newInfo.append(False)
-    stuck.append(False)
-    print("Local Map", i,"\n", World.robots[i].localMap)
+for col in range(sheet.ncols):
+    for row in range(sheet.nrows):
+        env1[row, col] = int(sheet.cell_value(row, col))
 
-trace = False
-count = 0
-#Run loop while any robots are not stuck
-while (False in stuck):
-    for i in range(robotCount):
+# env = [[ 1,  1,  1,  1,  1,  1,  1,  1,  1,  1],
+#       [ 1,  0,  0,  1,  0,  0,  0,  0,  0,  1],
+#       [ 1,  0,  0,  0,  0,  0,  0,  0,  0,  1],
+#       [ 1,  0,  0,  0,  0,  0,  0,  0,  0,  1],
+#       [ 1,  0,  0,  0,  0,  0,  0,  0,  0,  1],
+#       [ 1,  0,  0,  0,  0,  0,  0,  0,  0,  1],
+#       [ 1,  0,  0,  0,  0,  0,  0,  0,  0,  1],
+#       [ 1,  0,  1,  0,  1,  0,  0,  0,  0,  1],
+#       [ 1,  0,  0,  0,  0,  0,  1,  0,  0,  1],
+#       [ 1,  1,  1,  1,  1,  1,  1,  1,  1,  1]]
 
-        """Goals Update"""
-        #If the current goal is no longer unknown, get new goal. Goal initializes as origin.
-        if (World.robots[i].localMap[World.robots[i].goal[0]][World.robots[i].goal[1]] != 3) or newInfo[i]:
-            legalGoal = False
-            while not legalGoal and not stuck[i]:
-                #Are there any remaining goals?
-                if len(World.robots[i].goalsList) > 0:
-                    #Do we have new info but havnt reached the current goal?
-                    if not newInfo[i]:
-                        #Get new goal from goals list
-                        World.robots[i].goal = World.robots[i].getNextGoal()
-                    newInfo[i] = False
-                    #Is the current goal still unknown?
-                    if World.robots[i].localMap[World.robots[i].goal[0]][World.robots[i].goal[1]] == 3:
-                        #A* search to get path to goal
-                        Search0 = searchAStar(World.robots[i].location, World.robots[i].goal, World.robots[i].localMap)
-                        solution = Search0.solve()
-                        World.robots[i].currentPath = q.Queue()
-                        for step in solution:
-                            World.robots[i].currentPath.put(step)
-                            legalGoal = True
-                    # else:
-                    #     print("Removing goal: {}".format(World.robots[i].goal))
-                else:
-                    #If no remaining goals, consider the robot stuck
-                    stuck[i] = True
+Env1 = Environment((100,100), .25, 1, env=env1)
+print(Env1.envMatrix)
+# Env1.robots[0].updateMap(Env1.robotsLocation[0], Env1.envMatrix)
+# Env1.robots[0].getGoals()
+#
+# print("World Map:\n", Env1.envMatrix)
+# print("Local Map:\n", Env1.robots[0].localMap)
+#
+# trace = False
+# newInfo = False
+# stuck = False
+# while not stuck: #Loop until robot has no unknown locations
+#     #If the current goal is no longer unknown, get new goal. Goal initializes as origin.
+#     if (Env1.robots[0].localMap[Env1.robots[0].goal[0]][Env1.robots[0].goal[1]] != 3) or newInfo:
+#         legalGoal = False
+#         while not legalGoal and not stuck:
+#             if len(Env1.robots[0].goalsList) > 0:
+#                 if not newInfo:
+#                     Env1.robots[0].goal = Env1.robots[0].getNextGoal()
+#                 newInfo = False
+#                 if Env1.robots[0].localMap[Env1.robots[0].goal[0]][Env1.robots[0].goal[1]] == 3:
+#                     Search0 = searchAStar(Env1.robots[0].location, Env1.robots[0].goal, Env1.robots[0].localMap)
+#                     solution = Search0.solve()
+#                     Env1.robots[0].currentPath = q.Queue()
+#                     for i in solution:
+#                         Env1.robots[0].currentPath.put(i)
+#                         legalGoal = True
+#                 # else:
+#                 #     print("Removing goal: {}".format(Env1.robots[0].goal))
+#             else:
+#                 stuck = True
+#             # if not Env1.robots[0].goals.empty():
+#             #     Env1.robots[0].goal = Env1.robots[0].goals.get()
+#             #     if Env1.robots[0].localMap[Env1.robots[0].goal[0]][Env1.robots[0].goal[1]] == 3:
+#             #         Search0 = searchAStar(Env1.robots[0].location, Env1.robots[0].goal, Env1.robots[0].localMap)
+#             #         solution = Search0.solve()
+#             #         Env1.robots[0].currentPath = q.Queue()
+#             #         # print("Current Goal:", Env1.robots[0].goal)
+#             #         for i in solution:
+#             #             Env1.robots[0].currentPath.put(i)
+#             #         legalGoal = True
+#             # else:
+#             #     stuck = True
+#     if trace:
+#         print("World Map:\n", Env1.envMatrix)
+#         print("Local Map:\n", Env1.robots[0].localMap)
+#         print("World Location:", Env1.robotsLocation[0])
+#         print("Relative Location:", Env1.robots[0].location)
+#         print("Current Goal", Env1.robots[0].goal)
+#         print("Goals list", Env1.robots[0].goalsList)
+#         input('waiting...')
+#     nextMove = Env1.robots[0].currentPath.get()
+#     # print("World Map:\n", Env1.envMatrix)
+#     # print("Local Map:\n", Env1.robots[0].localMap)
+#     # print("Next Move:", nextMove)
+#     # print("World Location", Env1.robotsLocation[0])
+#     # print("Local Location", Env1.robots[0].location)
+#     if Env1.robots[0].localMap[nextMove[0]][nextMove[1]] == 0:
+#         direction = [nextMove[0] - Env1.robots[0].location[0], nextMove[1] - Env1.robots[0].location[1]]
+#         Env1.robots[0].visitedCells.append(Env1.robots[0].location)
+#         Env1.robots[0].move(nextMove)
+#         Env1.updateEnvMatrix(0, direction)
+#         Env1.robots[0].updateMap(Env1.robotsLocation[0], Env1.envMatrix)
+#         Env1.robots[0].getGoals()
+#         # goalCheck = Env1.robots[0].localMap[Env1.robots[0].goal[0]][Env1.robots[0].goal[1]] == 3
+#     else:
+#         newInfo = True
+#         # Env1.robots[0].goals.put(Env1.robots[0].goal)
+#
+#     # goalCheck = Env1.robots[0].localMap[Env1.robots[0].goal[0]][Env1.robots[0].goal[1]] == 3
+#     # while goalCheck and not stuck:
+#     #     nextMove = Env1.robots[0].currentPath.get()
+#     #     print("World Map:\n", Env1.envMatrix)
+#     #     print("Local Map:\n", Env1.robots[0].localMap)
+#     #     print("Next Move:", nextMove)
+#     #     print("World Location", Env1.robotsLocation[0])
+#     #     print("Local Location", Env1.robots[0].location)
+#     #     if Env1.robots[0].localMap[nextMove[0]][nextMove[1]] == 0:
+#     #         direction = [nextMove[0] - Env1.robots[0].location[0], nextMove[1] - Env1.robots[0].location[1]]
+#     #         Env1.robots[0].visitedCells.append(Env1.robots[0].location)
+#     #         Env1.robots[0].move(nextMove)
+#     #         Env1.updateEnvMatrix(0, direction)
+#     #         Env1.robots[0].updateMap(Env1.robotsLocation[0], Env1.envMatrix)
+#     #         goalCheck = Env1.robots[0].localMap[Env1.robots[0].goal[0]][Env1.robots[0].goal[1]] == 3
+#     #     else:
+#     #         goalCheck = False
+#     #
+#     #     # input("Waiting")
+#     # Env1.robots[0].currentPath = q.Queue()
+# print("World Map:\n", Env1.envMatrix)
+# print("Local Map:\n", Env1.robots[0].localMap)
 
-        """Movement update"""
-        if not stuck[i]:
-            nextMove = World.robots[i].currentPath.get()
-            #Only move to the next location if it is empty
-            if World.robots[i].localMap[nextMove[0]][nextMove[1]] == 0:
-                direction = [nextMove[0] - World.robots[i].location[0], nextMove[1] - World.robots[i].location[1]]
-                World.robots[i].move(nextMove)
-                World.updateEnvMatrix(i, direction)
-                World.robots[i].updateMap(World.robotsLocation[i], World.envMatrix)
-                World.robots[i].getGoals()
-            else:
-                newInfo[i] = True
-    if trace:
-        print("World Map:\n", World.envMatrix)
-        for i in range(robotCount):
-            print("Local Map", i,"\n", World.robots[i].localMap)
-        input('waiting')
-print("World Map:\n", World.envMatrix)
-for i in range(robotCount):
-    print("Local Map", i,"\n", World.robots[i].localMap)
-
-
-
-"""Code below this line works for one robot only"""
-"""#Initilize environment (SIZE, Wall%, # of Robots)
-Env1 = Environment((96, 96), .2, 1)
-Env1.robots[0].updateMap(Env1.robotsLocation[0], Env1.envMatrix)
-Env1.robots[0].getGoals()
-
-print("World Map:\n", Env1.envMatrix)
-print("Local Map:\n", Env1.robots[0].localMap)
-
-trace = False
-newInfo = False
-stuck = False
-while not stuck: #Loop until robot has no unknown locations
-
-
-    #If the current goal is no longer unknown, get new goal. Goal initializes as origin.
-    if (Env1.robots[0].localMap[Env1.robots[0].goal[0]][Env1.robots[0].goal[1]] != 3) or newInfo:
-        legalGoal = False
-        while not legalGoal and not stuck:
-            if len(Env1.robots[0].goalsList) > 0:
-                if not newInfo:
-                    Env1.robots[0].goal = Env1.robots[0].getNextGoal()
-                newInfo = False
-                if Env1.robots[0].localMap[Env1.robots[0].goal[0]][Env1.robots[0].goal[1]] == 3:
-                    Search0 = searchAStar(Env1.robots[0].location, Env1.robots[0].goal, Env1.robots[0].localMap)
-                    solution = Search0.solve()
-                    Env1.robots[0].currentPath = q.Queue()
-                    for i in solution:
-                        Env1.robots[0].currentPath.put(i)
-                        legalGoal = True
-                # else:
-                #     print("Removing goal: {}".format(Env1.robots[0].goal))
-            else:
-                stuck = True
-
-    if trace:
-        print("World Map:\n", Env1.envMatrix)
-        print("Local Map:\n", Env1.robots[0].localMap)
-        print("World Location:", Env1.robotsLocation[0])
-        print("Relative Location:", Env1.robots[0].location)
-        print("Current Goal", Env1.robots[0].goal)
-        print("Goals list", Env1.robots[0].goalsList)
-        input('waiting...')
-    nextMove = Env1.robots[0].currentPath.get()
-    if Env1.robots[0].localMap[nextMove[0]][nextMove[1]] == 0:
-        direction = [nextMove[0] - Env1.robots[0].location[0], nextMove[1] - Env1.robots[0].location[1]]
-        Env1.robots[0].move(nextMove)
-        Env1.updateEnvMatrix(0, direction)
-        Env1.robots[0].updateMap(Env1.robotsLocation[0], Env1.envMatrix)
-        Env1.robots[0].getGoals()
-    else:
-        newInfo = True
-
-print("World Map:\n", Env1.envMatrix)
-print("Local Map:\n", Env1.robots[0].localMap)"""
+# print(Env1.robots[0].localMap)
+# while not Env1.robots[0].goals.empty():
+#     print (Env1.robots[0].goals.get())
+# input("DONE")
+# print("Robot World Location: ", Env1.robotsLocation[0])
+# print("Robot Relative Location: ", Env1.robots[0].location)
+# print("Robot Local Map:\n", Env1.robots[0].localMap)
+# stuck = False
+# while not stuck:
+#     Env1.robots[0].updateMap(Env1.robotsLocation[0], Env1.envMatrix)
+#     if Env1.robots[0].currentPath.empty():
+#         # input("IN NEW PATH SEARCH...")
+#         solution = Env1.robots[0].getClosestUnknown()
+#         if solution == None:
+#             stuck = True
+#         else:
+#             for i in solution:
+#                 Env1.robots[0].currentPath.put(i)
+#
+#     # iBump = 0
+#     # jBump = 0
+#     while (Env1.robots[0].localMap[Env1.robots[0].goal[0]][Env1.robots[0].goal[1]] == 3) and (not Env1.robots[0].currentPath.empty()) and not stuck:
+#         # print("Robot World Location: ", Env1.robotsLocation[0])
+#         # print("Robot Relative Location: ", Env1.robots[0].location)
+#         # print("Robot Local Map:\n", Env1.robots[0].localMap)
+#         # print("Path to goal,", Env1.robots[0].goal, ",\n", solution)
+#         # input("waiting...")
+#         nextMove = Env1.robots[0].currentPath.get()
+#         # print(Env1.robots[0].localMap.size)
+#         # print("nextMove:", nextMove)
+#         # nextMove[0] += iBump
+#         # nextMove[1] += jBump
+#         if Env1.robots[0].localMap[nextMove[0]][nextMove[1]] == 0:
+#             direction = [nextMove[0] - Env1.robots[0].location[0], nextMove[1] - Env1.robots[0].location[1]]
+#             Env1.robots[0].move(nextMove)
+#             Env1.updateEnvMatrix(0, direction)
+#             Env1.robots[0].updateMap(Env1.robotsLocation[0], Env1.envMatrix)
+#         else:
+#             Env1.robots[0].currentPath = q.Queue()
+#         # iBump += iBumpDiff
+#         # jBump += jBumpDiff
+#     Env1.robots[0].currentPath = q.Queue()
+#
+# print(Env1.robots[0].localMap)
+# print(Env1.envMatrix)
+#
+#
+#
+#
+#
+#
+# # for i in range(50):
+# #     Env1.robots[0].updateMap(Env1.robotsLocation[0], Env1.envMatrix)
+# #     # print("Robot World Location: ", Env1.robotsLocation[0])
+# #     # print("Robot Relative Location: ", Env1.robots[0].location)
+# #     # print("Robot Local Map:\n", Env1.robots[0].localMap)
+# #     legalMoves = Env1.robots[0].getLegalMoves(Env1.robots[0].location)
+# #     choice = random.choice(legalMoves)
+# #     direction = [choice[0] - Env1.robots[0].location[0], choice[1] - Env1.robots[0].location[1]]
+# #     # print("Direction: ", direction)
+# #     Env1.robots[0].move(choice)
+# #     Env1.updateEnvMatrix(0, direction)
+# #     # print("World Map:\n", Env1.envMatrix)
+# # Env1.robots[0].updateMap(Env1.robotsLocation[0], Env1.envMatrix)
+# # print("Robot Local Map:\n", Env1.robots[0].localMap)
+# # print("Closest Unknown", Env1.robots[0].getClosestUnknown())
+# # print("Robot Relative Location: ", Env1.robots[0].location)
+# # input('waiting...')
+# # Env1.robots[0].goal = Env1.robots[0].getClosestUnknown()
+# # search0 = searchAStar(Env1.robots[0].location, Env1.robots[0].goal, Env1.robots[0].localMap)
+# # Env1.robots[0].currentPath = search0.solve()
+# # print(Env1.robots[0].currentPath)
+#
+#
+#
+# # for i in range(0, len(Env1.robots)):
+# #     print("Robot", i, "World Location: ", Env1.robotsLocation[i])
+# #     print("Robot", i, "Relative Location: ", Env1.robots[i].location)
+# #     Env1.robots[i].updateMap(Env1.robotsLocation[i], Env1.envMatrix)
+# #     print("Robot", i, "Local Map After Update:\n", Env1.robots[i].localMap)
+# #     legalMoves = Env1.robots[i].getLegalMoves()
+# #     print("Robot", i, "Legal Moves:", legalMoves)
+#     Env1.robots[i].move(legalMoves[0])
+#     Env1.robots[i].updateMap(Env1.robotsLocation[i], Env1.envMatrix)
+#     print("Robot", i, "Local Map After Move and Update\n", Env1.robots[i].localMap)
+#     print("Robot", i, "Relative Location: ", Env1.robots[i].location)

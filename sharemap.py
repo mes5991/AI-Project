6818fc8f328e5d72bb6eeb3cf44f7910d334b9e7
syamprasadkr <syamprasadkr@gmail.com>
2016-04-14 10:25:59 -0400

    Updated Share map function
    Requires two maps and a direction indicator

--- sharemap.py
+++ sharemap.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def shareMap(robot1,robot2,dir1,dir2):
+def shareMap(robot1,robot2,dir1):
     item=2 # Location of the other robot
 
     #arr1=robot1().localMap
@@ -8,19 +8,45 @@ def shareMap(robot1,robot2,dir1,dir2):
     #if (dir1==2 and dir2==0) or (dir1==0 and dir2==2):
 
     # Assigns arrays according to relative locations of robot
-    if dir1==2 and dir2==0:
+    if dir1==2:
         arr1=robot1
         arr2=robot2
-    elif dir1==0 and dir2==2:
+
+        index1=np.where(arr1==2)
+        index2=np.where(arr2==2)
+        arr1[index1[0][0]][index1[1][0]]=0
+        arr1[index1[0][0]+1][index1[1][0]]=2
+        arr2[index2[0][0]][index2[1][0]]=0
+        arr2[index2[0][0]-1][index2[1][0]]=2
+    elif dir1==0:
         arr1=robot2
         arr2=robot1
-    elif dir1==1 and dir2==3:
+        index1=np.where(arr1==2)
+        index2=np.where(arr2==2)
+        arr1[index1[0][0]][index1[1][0]]=0
+        arr1[index1[0][0]+1][index1[1][0]]=2
+        arr2[index2[0][0]][index2[1][0]]=0
+        arr2[index2[0][0]-1][index2[1][0]]=2
+    elif dir1==1:
         arr1=np.transpose(robot1)
         arr2=np.transpose(robot2)
-    elif dir1==3 and dir2==1:
+        index1=np.where(arr1==2)
+        index2=np.where(arr2==2)
+        arr1[index1[0][0]][index1[1][0]]=0
+        arr1[index1[0][0]+1][index1[1][0]]=2
+        arr2[index2[0][0]][index2[1][0]]=0
+        arr2[index2[0][0]-1][index2[1][0]]=2
+    elif dir1==3:
         arr1=np.transpose(robot2)
         arr2=np.transpose(robot1)
+        index1=np.where(arr1==2)
+        index2=np.where(arr2==2)
+        arr1[index1[0][0]][index1[1][0]]=0
+        arr1[index1[0][0]+1][index1[1][0]]=2
+        arr2[index2[0][0]][index2[1][0]]=0
+        arr2[index2[0][0]+1][index2[1][0]]=2
     # 1 Ignore this comment
+
     size1=arr1.shape
     size2=arr2.shape
     # 2 Ignore this comment
@@ -32,11 +58,11 @@ def shareMap(robot1,robot2,dir1,dir2):
     if size2[1]>size1[1]:
         for n in range(sizcol):
             arr1 = np.insert(arr1,size1[1],3,1)
-    print("Array1\n",arr1)
+    #print("Array1\n",arr1)
     if size1[1]>size2[1]:
         for n in range(sizcol):
             arr2 = np.insert(arr2,size2[1],3,1)
-    print("Array2\n",arr2)
+    #print("Array2\n",arr2)
     itemindex1=np.where(arr1==item)
     itemindex2=np.where(arr2==item)
     #print((itemindex1[0][0],itemindex1[1][0]),(itemindex2[0][0],itemindex2[1][0]))
@@ -79,31 +105,32 @@ def shareMap(robot1,robot2,dir1,dir2):
                 arr1[itemindex1[0][0]+1+i][j]=arr2[itemindex2[0][0]+2+i][j]
             elif arr2[itemindex2[0][0]+2+i][j]==3:
                 arr2[itemindex2[0][0]+2+i][j] = arr1[itemindex1[0][0]+1+i][j]
-    print("Array1\n",arr1)
-    print("Array2\n",arr2)
+    #print("Array1\n",arr1)
+    #print("Array2\n",arr2)
 
     # Return Values
-    if dir1==2 and dir2==0:
+    if dir1==2:
         return arr2,arr1
-    elif dir1==0 and dir2==2:
+    elif dir1==0:
         return arr1,arr2
-    elif dir1==1 and dir2==3:
+    elif dir1==1:
         return np.transpose(arr2),np.transpose(arr1)
-    elif dir1==3 and dir2==1:
+    elif dir1==3:
         return np.transpose(arr1),np.transpose(arr2)
     # 4 Ignore this comment
 
 # Test the code
 # Uncomment to test the following case
-'''robot1=np.array([[0,1,3,3],[0,0,0,0],[1,1,0,3],[3,3,0,3],[0,0,2,0],[3,3,3,1],[3,3,3,3],[0,0,0,1]])
-robot2=np.array([[0,0,2,0],[0,0,0,0],[1,1,0,3],[1,1,1,1]])
-(array1,array2)=shareMap(robot1,robot2,2,0)
+robot1=np.array([[0,1,3,3],[0,0,0,0],[1,1,0,3],[3,3,2,3],[0,0,0,0],[3,3,3,1],[3,3,3,3],[0,0,0,1]])
+robot2=np.array([[0,0,0,0],[0,0,2,0],[1,1,0,3],[1,1,1,1]])
+(array1,array2)=shareMap(robot1,robot2,2)
 print("Array1\n",array1)
-print("Array2\n",array2)'''
+print("Array2\n",array2)
 
 # Uncomment to test the following case
-'''robot1=np.array([[0,1,3,3],[0,0,0,0],[1,1,0,2],[3,1,0,3],[0,0,1,0]])
-robot2=np.array([[1,3,0,0],[0,3,0,0],[1,2,0,0],[1,0,1,0]])
-(array1,array2)=shareMap(robot1,robot2,1,3)
+'''robot1=np.array([[0,1,3,3],[0,0,0,0],[1,1,2,0],[3,1,0,3],[0,0,1,0]])
+robot2=np.array([[1,3,0,0],[0,3,0,0],[1,0,2,0],[1,0,1,0]])
+#print(robot1,robot2)
+(array1,array2)=shareMap(robot1,robot2,1)
 print("Array1\n",array1)
 print("Array2\n",array2)'''
